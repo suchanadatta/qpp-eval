8636dc55f6a3ae1924a1b400f0ec6468476732f4
Debasis Ganguly <debforit@gmail.com>
2022-02-02 13:37:33 +0000

    adding modified class files so that i can remove them in the next commit

--- qppeval.sh
+++ qppeval.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-if [ $# -lt 6 ]
+if [ $# -lt 3 ]
 then
     echo "Usage: " $0 " <following arguments in sequence>";
-    echo "1. Num docs to retrieve (e.g. 100)";
-    echo "2. Num-top docs for qpp-estimation (e.g. 50)";
-    echo "3. Method (avgidf/nqc/wig/clarity/uef_nqc,/uef_wig/uef_clarity)";
-    echo "4. Retrieval Eval Metric (ap, p_10, recall, ndcg)";
-    echo "5. QPP eval Metric (r/rho/tau/qsim/qsim_strict/pairacc/class_acc/rmse)";
-    echo "6. % of train query set (an integer between [0, 100])";
+    echo "1. Method (avgidf/nqc/wig/clarity/uef_nqc,/uef_wig/uef_clarity)";
+    echo "2. Retrieval Eval Metric (ap, p_10, recall, ndcg)";
+    echo "3. QPP eval Metric (r/rho/tau/qsim/qsim_strict/pairacc/class_acc/rmse)";
     exit 1;
 fi
 
-METHOD=$3
-RETEVAL_METRIC=$4
-METRIC=$5
-SPLITS=$6
+#These are hyper-parameters... usually these values work well
+NUMWANTED=100
+NUMTOP=50
+SPLITS=80
+
+METHOD=$1
+RETEVAL_METRIC=$2
+METRIC=$3
 INDEXDIR=/Users/debasis/research/common/trecd45/index/
 QRELS=data/qrels.robust.all
 QUERYFILE=data/topics.robust.all
@@ -28,9 +29,9 @@ qrels.file=$QRELS
 res.file=outputs/lmdir.res
 res.train=outputs/train.res
 res.test=outputs/test.res
-retrieve.num_wanted=$1
+retrieve.num_wanted=$NUMWANTED
 reteval.metric=$RETEVAL_METRIC
-qpp.numtopdocs=$2
+qpp.numtopdocs=$NUMTOP
 qpp.method=$METHOD
 qpp.metric=$METRIC
 qpp.splits=$SPLITS
@@ -41,6 +42,6 @@ EOF1
 #mvn exec:java@compute_all -Dexec.args="qpp.properties"
 #mvn exec:java@across_metrics -Dexec.args="qpp.properties"
 #mvn exec:java@across_models -Dexec.args="qpp.properties"
-mvn exec:java@linear_regressor -Dexec.args="qpp.properties"
+#mvn exec:java@linear_regressor -Dexec.args="qpp.properties"
 #mvn exec:java@poly_regressor -Dexec.args="qpp.properties"
 
